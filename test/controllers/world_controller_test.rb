6f0d77941327ca884e84b71fc81cb1f322e65fc6
Travis Tucker <t.tuck303@gmail.com>
2016-03-01 11:29:26 -0700

    Basic testing for hitboxes.

--- test/controllers/world_controller_test.rb
+++ test/controllers/world_controller_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class WorldControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+
 
   test "should get index" do
     get :index
     assert_response :success
   end
+
+  # for this test to work, may need to set the instance variable first
+  # test "should get show" do
+  #   get :show
+  #   assert_response :success
+  # end
 
 end
--- test/models/hitbox_test.rb
+++ test/models/hitbox_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class HitboxTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @hitbox = Hitbox.new(world_id: 1, character: "Waldo", x_left: 34.4, x_right: 35.4, y_top: 23.3, y_bottom: 25.6)
+  end
+
+  test "should be valid" do
+    assert @hitbox.valid?
+  end
+
+  test "world should be present" do
+    @hitbox.world_id = nil
+    assert_not @hitbox.valid?
+  end
+
+  test "coordinates should be present" do
+    @hitbox.x_left = nil
+    assert_not @hitbox.valid?
+  end
+
+  test "character should be one of Waldo, Odlaw, or Wizard" do
+    @hitbox.character = "Steve"
+    assert_not @hitbox.valid?
+  end
+
+  test "the truth" do
+    assert true
+  end
 end
