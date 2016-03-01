ba80762ce93ad8fcdca3d490b0e25a30db404293
Travis Tucker <t.tuck303@gmail.com>
2016-03-01 15:49:31 -0700

    Implements buggy character selector menu.

--- app/assets/javascripts/waldo_finder.js
+++ app/assets/javascripts/waldo_finder.js
@@ -1,8 +1,10 @@
 $(function(){
   console.log("jQuery loaded and ready");
+  var char_modal = $(".character_select");
+  char_modal.hide();
   $(".t_large_image_holder img").click(function(e){
+    char_modal.offset({top: e.pageY, left: e.pageX}).show();
      var parentOffset = $(this).parent().offset(); 
-     //or $(this).offset(); if you really just want the current element's offset
      var relX = e.pageX - parentOffset.left;
      var relY = e.pageY - parentOffset.top;
      console.log("Image was clicked.");
@@ -11,6 +13,11 @@ $(function(){
      console.log("relY: "+ relY);
      console.log("pageX: " + e.pageX);
      console.log("pageY: " + e.pageY);
-     $(".character_select").offset({top: e.pageY, left: e.pageX}).css("opacity", 1);
+     console.log("showing modal");
+  });
+  $("#nevermind").click(function(e){
+    console.log("hiding modal");
+    e.preventDefault();
+    char_modal.hide();
   });
 });
