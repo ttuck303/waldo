ccf9a53064e8fed852af6e00ed918d26cf963c2c
Travis Tucker <t.tuck303@gmail.com>
2016-03-08 21:36:23 -0700

    Restored worlds and hitbox for space.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@ World.create!([
   {title: "Gobbling Gluttons", difficulty: "Hard", image_file_name: "open-uri20160308-13329-1smrbdn", image_content_type: "image/jpeg", image_file_size: 1633594, image_updated_at: "2016-03-09 04:08:26"},
   {title: "Toys, Toys, Toys!", difficulty: "Hard", image_file_name: "open-uri20160308-13590-1dbeg2l", image_content_type: "image/jpeg", image_file_size: 2333392, image_updated_at: "2016-03-09 04:16:54"}
 ])
+
+Hitbox.create!([
+  {world_id: 1, character: "Waldo", x_left: "398.9", x_right: "409.95", y_bottom: "420.48", y_top: "400.48"},
+  {world_id: 1, character: "Whitebeard", x_left: "773.95", x_right: "787.95", y_bottom: "386.48", y_top: "361.48"},
+  {world_id: 1, character: "Odlaw", x_left: "64.9", x_right: "76.95", y_bottom: "468.484375", y_top: "449.48"}
+])
