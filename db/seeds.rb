ad35f5b79bed131b67cfe57ca5216e122990b39d
Travis Tucker <t.tuck303@gmail.com>
2016-03-09 19:01:03 -0700

    Adds hitboxes for Ancient Rome and puts them into seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,19 @@ World.create!([
 Hitbox.create!([
   {world_id: 1, character: "Waldo", x_left: "398.9", x_right: "409.95", y_bottom: "420.48", y_top: "400.48"},
   {world_id: 1, character: "Whitebeard", x_left: "773.95", x_right: "787.95", y_bottom: "386.48", y_top: "361.48"},
-  {world_id: 1, character: "Odlaw", x_left: "64.9", x_right: "76.95", y_bottom: "468.484375", y_top: "449.48"}
+  {world_id: 1, character: "Odlaw", x_left: "64.9", x_right: "76.95", y_bottom: "468.484375", y_top: "449.48"},
+  {world_id: 2, character: "Waldo", x_left: "406.95", x_right: "424.95", y_bottom: "204.48", y_top: "170.48"},
+  {world_id: 2, character: "Wenda", x_left: "955.95", x_right: "966.95", y_bottom: "69.48", y_top: "52.48"},
+  {world_id: 2, character: "Whitebeard", x_left: "446.95", x_right: "466.95", y_bottom: "121.48", y_top: "89.48"},
+  {world_id: 2, character: "Odlaw", x_left: "40.95", x_right: "52.95", y_bottom: "472.28", y_top: "449.48"}
+])
+
+Highscore.create!([
+  {name: "travis", time: 47, world_id: 1},
+  {name: "tr@verz", time: 77, world_id: 1},
+  {name: "TravIs D. Bestever", time: 19, world_id: 1},
+  {name: "Guest", time: 11, world_id: 1},
+  {name: "Guest", time: 15, world_id: 1},
+  {name: "TravIz Dee Best", time: 13, world_id: 1},
+  {name: "Trav is #1", time: 11, world_id: 1}
 ])
