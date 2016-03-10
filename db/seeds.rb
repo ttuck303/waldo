288fce4a42f0e2f7616c3beb2e7fa640e3fbcdbb
Travis Tucker <t.tuck303@gmail.com>
2016-03-10 13:04:49 -0700

    Fixes seeds again so that hitboxes match up properply and images are included.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 World.create!([
-  {title: "Gobbling Gluttons", difficulty: "Hard", image_file_name: "open-uri20160308-13329-1smrbdn", image_content_type: "image/jpeg", image_file_size: 1633594, image_updated_at: "2016-03-09 04:08:26"},
-  {title: "Space", difficulty: "Medium", image_file_name: "open-uri20160308-13329-1ge5be7", image_content_type: "image/jpeg", image_file_size: 2848301, image_updated_at: "2016-03-09 04:01:12"},
-  {title: "Ancient Rome", difficulty: "Easy", image_file_name: "open-uri20160308-13329-hporpg", image_content_type: "image/jpeg", image_file_size: 2324513, image_updated_at: "2016-03-09 04:02:00"},
-  {title: "Merry Mess-Up", difficulty: "Legendary", image_file_name: "open-uri20160308-13329-1xqtscu", image_content_type: "image/jpeg", image_file_size: 3181556, image_updated_at: "2016-03-09 04:06:18"},
-  {title: "Castle", difficulty: "Hard", image_file_name: "open-uri20160308-13329-1ypfow2", image_content_type: "image/jpeg", image_file_size: 2961649, image_updated_at: "2016-03-09 04:07:16"},
-  {title: "Toys, Toys, Toys!", difficulty: "Very Hard", image_file_name: "open-uri20160308-13590-1dbeg2l", image_content_type: "image/jpeg", image_file_size: 2333392, image_updated_at: "2016-03-09 04:16:54"}
+  {title: "Space", difficulty: "Medium", image: open("https://s3.amazonaws.com/wheres-waldo/+waldo+-+space.jpg") },
+  {title: "Ancient Rome", difficulty: "Easy", image: open("https://s3.amazonaws.com/wheres-waldo/waldo+-+ancient+rome.jpg") },
+  {title: "Merry Mess-Up", difficulty: "Legendary", image: open("https://s3.amazonaws.com/wheres-waldo/waldo+-+castle.jpg") },
+  {title: "Castle", difficulty: "Hard", image: open("https://s3.amazonaws.com/wheres-waldo/waldo+-+castle+2.jpg") },
+  {title: "Gobbling Gluttons", difficulty: "Hard", image: open("https://s3.amazonaws.com/wheres-waldo/waldo+-+gobbling+gluttons.jpg") },
+  {title: "Toys, Toys, Toys!", difficulty: "Very Hard", image: open("https://s3.amazonaws.com/wheres-waldo/waldo+-+toys.jpg")}
 ])
 
 Hitbox.create!([
