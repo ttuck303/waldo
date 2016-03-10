9aad486ff701ef1aec3505299ee07170ef2ba015
Travis Tucker <t.tuck303@gmail.com>
2016-03-10 12:40:47 -0700

    Gems and precompile to prepare for heroku push.

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'aws-sdk', '< 2.0'
 gem 'figaro'
 gem 'jquery-turbolinks'
 gem 'seed_dump'
+gem 'rails_12factor', group: :production
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-Highscore.create!([
-  {name: "travis", time: 47, world_id: 1},
-  {name: "tr@verz", time: 77, world_id: 1},
-  {name: "TravIs D. Bestever", time: 19, world_id: 1},
-  {name: "Guest", time: 11, world_id: 1},
-  {name: "Guest", time: 15, world_id: 1},
-  {name: "TravIz Dee Best", time: 13, world_id: 1},
-  {name: "Trav is #1", time: 11, world_id: 1},
-  {name: "travisimo", time: 14, world_id: 2}
+World.create!([
+  {title: "Gobbling Gluttons", difficulty: "Hard", image_file_name: "open-uri20160308-13329-1smrbdn", image_content_type: "image/jpeg", image_file_size: 1633594, image_updated_at: "2016-03-09 04:08:26"},
+  {title: "Space", difficulty: "Medium", image_file_name: "open-uri20160308-13329-1ge5be7", image_content_type: "image/jpeg", image_file_size: 2848301, image_updated_at: "2016-03-09 04:01:12"},
+  {title: "Ancient Rome", difficulty: "Easy", image_file_name: "open-uri20160308-13329-hporpg", image_content_type: "image/jpeg", image_file_size: 2324513, image_updated_at: "2016-03-09 04:02:00"},
+  {title: "Merry Mess-Up", difficulty: "Legendary", image_file_name: "open-uri20160308-13329-1xqtscu", image_content_type: "image/jpeg", image_file_size: 3181556, image_updated_at: "2016-03-09 04:06:18"},
+  {title: "Castle", difficulty: "Hard", image_file_name: "open-uri20160308-13329-1ypfow2", image_content_type: "image/jpeg", image_file_size: 2961649, image_updated_at: "2016-03-09 04:07:16"},
+  {title: "Toys, Toys, Toys!", difficulty: "Very Hard", image_file_name: "open-uri20160308-13590-1dbeg2l", image_content_type: "image/jpeg", image_file_size: 2333392, image_updated_at: "2016-03-09 04:16:54"}
 ])
+
 Hitbox.create!([
   {world_id: 1, character: "Waldo", x_left: "398.9", x_right: "409.95", y_bottom: "420.48", y_top: "400.48"},
   {world_id: 1, character: "Whitebeard", x_left: "773.95", x_right: "787.95", y_bottom: "386.48", y_top: "361.48"},
@@ -34,11 +33,14 @@ Hitbox.create!([
   {world_id: 4, character: "Whitebeard", x_left: "283.95", x_right: "290.95", y_bottom: "89.48", y_top: "81.48"},
   {world_id: 1, character: "Wenda", x_left: "287.95", x_right: "298.95", y_bottom: "354.48", y_top: "333.48"}
 ])
-World.create!([
-  {title: "Gobbling Gluttons", difficulty: "Hard", image_file_name: "open-uri20160308-13329-1smrbdn", image_content_type: "image/jpeg", image_file_size: 1633594, image_updated_at: "2016-03-09 04:08:26"},
-  {title: "Space", difficulty: "Medium", image_file_name: "open-uri20160308-13329-1ge5be7", image_content_type: "image/jpeg", image_file_size: 2848301, image_updated_at: "2016-03-09 04:01:12"},
-  {title: "Ancient Rome", difficulty: "Easy", image_file_name: "open-uri20160308-13329-hporpg", image_content_type: "image/jpeg", image_file_size: 2324513, image_updated_at: "2016-03-09 04:02:00"},
-  {title: "Merry Mess-Up", difficulty: "Legendary", image_file_name: "open-uri20160308-13329-1xqtscu", image_content_type: "image/jpeg", image_file_size: 3181556, image_updated_at: "2016-03-09 04:06:18"},
-  {title: "Castle", difficulty: "Hard", image_file_name: "open-uri20160308-13329-1ypfow2", image_content_type: "image/jpeg", image_file_size: 2961649, image_updated_at: "2016-03-09 04:07:16"},
-  {title: "Toys, Toys, Toys!", difficulty: "Very Hard", image_file_name: "open-uri20160308-13590-1dbeg2l", image_content_type: "image/jpeg", image_file_size: 2333392, image_updated_at: "2016-03-09 04:16:54"}
+
+Highscore.create!([
+  {name: "travis", time: 47, world_id: 1},
+  {name: "tr@verz", time: 77, world_id: 1},
+  {name: "TravIs D. Bestever", time: 19, world_id: 1},
+  {name: "Guest", time: 11, world_id: 1},
+  {name: "Guest", time: 15, world_id: 1},
+  {name: "TravIz Dee Best", time: 13, world_id: 1},
+  {name: "Trav is #1", time: 11, world_id: 1},
+  {name: "travisimo", time: 14, world_id: 2}
 ])
